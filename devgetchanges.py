microting/eform-angular-work-orders-plugin

Add cross-platform Python versions of shell scripts

devgetchanges.py
@@ -1,0 +1,65 @@
+import os
+import shutil
+
+# Change directory to the user's home directory
+os.chdir(os.path.expanduser("~"))
+
+# Define paths
+src_base = os.path.join("Documents", "workspace", "microting", "eform-angular-frontend")
+dst_base = os.path.join("Documents", "workspace", "microting", "eform-angular-work-orders-plugin")
+
+# Remove and copy main directories
+paths = [
+    (os.path.join("eform-client", "src", "app", "plugins", "modules", "workorders-pn"),
+     os.path.join("eform-client", "src", "app", "plugins", "modules", "workorders-pn")),
+    (os.path.join("eFormAPI", "Plugins", "WorkOrders.Pn"),
+     os.path.join("eFormAPI", "Plugins", "WorkOrders.Pn")),
+]
+
+for dst_rel_path, src_rel_path in paths:
+    dst_path = os.path.join(dst_base, dst_rel_path)
+    src_path = os.path.join(src_base, src_rel_path)
+
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+
+    shutil.copytree(src_path, dst_path)
+
+# List of test files and directories to remove
+test_files_to_remove = [
+    os.path.join("eform-client", "e2e", "Tests", "workorders-settings"),
+    os.path.join("eform-client", "e2e", "Tests", "workorders-general"),
+    os.path.join("eform-client", "wdio-headless-plugin-step2.conf.js"),
+    os.path.join("eform-client", "e2e", "Page objects", "WorkOrders"),
+]
+
+# Remove the test files and directories
+for rel_path in test_files_to_remove:
+    full_path = os.path.join(dst_base, rel_path)
+    if os.path.exists(full_path):
+        if os.path.isdir(full_path):
+            shutil.rmtree(full_path)
+        else:
+            os.remove(full_path)
+
+# List of test files and directories to copy
+test_files_to_copy = [
+    (os.path.join("eform-client", "e2e", "Tests", "workorders-settings"),
+     os.path.join("eform-client", "e2e", "Tests", "workorders-settings")),
+    (os.path.join("eform-client", "e2e", "Tests", "workorders-general"),
+     os.path.join("eform-client", "e2e", "Tests", "workorders-general")),
+    (os.path.join("eform-client", "wdio-plugin-step2.conf.js"),
+     os.path.join("eform-client", "wdio-headless-plugin-step2.conf.js")),
+    (os.path.join("eform-client", "e2e", "Page objects", "WorkOrders"),
+     os.path.join("eform-client", "e2e", "Page objects", "WorkOrders")),
+]
+
+# Copy the test files and directories
+for src_rel_path, dst_rel_path in test_files_to_copy:
+    src_path = os.path.join(src_base, src_rel_path)
+    dst_path = os.path.join(dst_base, dst_rel_path)
+
+    if os.path.isdir(src_path):
+        shutil.copytree(src_path, dst_path)
+    else:
+        shutil.copy2(src_path, dst_path)

devinstall.py
@@ -1,0 +1,75 @@
+import os
+import shutil
+
+# Change directory to the user's home directory
+os.chdir(os.path.expanduser("~"))
+
+# Print the current working directory
+print("Current working directory:", os.getcwd())
+
+# Define paths
+src_base = os.path.join("Documents", "workspace", "microting", "eform-angular-work-orders-plugin")
+dst_base = os.path.join("Documents", "workspace", "microting", "eform-angular-frontend")
+
+# Remove and copy directories
+paths = [
+    (os.path.join("eform-client", "src", "app", "plugins", "modules", "workorders-pn"),
+     os.path.join("eform-client", "src", "app", "plugins", "modules", "workorders-pn")),
+    (os.path.join("eFormAPI", "Plugins", "WorkOrders.Pn"),
+     os.path.join("eFormAPI", "Plugins", "WorkOrders.Pn")),
+]
+
+# Removing and copying files
+for dst_rel_path, src_rel_path in paths:
+    dst_path = os.path.join(dst_base, dst_rel_path)
+    src_path = os.path.join(src_base, src_rel_path)
+
+    # Remove the destination directory if it exists
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+
+    # Copy the source directory to the destination
+    shutil.copytree(src_path, dst_path)
+
+# Ensure the Plugins directory exists
+plugins_dir = os.path.join(dst_base, "eFormAPI", "Plugins")
+os.makedirs(plugins_dir, exist_ok=True)
+
+# List of test files and directories to remove
+test_files_to_remove = [
+    os.path.join("eform-client", "e2e", "Tests", "workorders-settings"),
+    os.path.join("eform-client", "e2e", "Tests", "workorders-general"),
+    os.path.join("eform-client", "e2e", "Page objects", "WorkOrders"),
+    os.path.join("eform-client", "wdio-plugin-step2.conf.js"),
+]
+
+# Remove the test files and directories
+for rel_path in test_files_to_remove:
+    full_path = os.path.join(dst_base, rel_path)
+    if os.path.exists(full_path):
+        if os.path.isdir(full_path):
+            shutil.rmtree(full_path)
+        else:
+            os.remove(full_path)
+
+# List of test files and directories to copy
+test_files_to_copy = [
+    (os.path.join("eform-client", "e2e", "Tests", "workorders-settings"),
+     os.path.join("eform-client", "e2e", "Tests", "workorders-settings")),
+    (os.path.join("eform-client", "e2e", "Tests", "workorders-general"),
+     os.path.join("eform-client", "e2e", "Tests", "workorders-general")),
+    (os.path.join("eform-client", "e2e", "Page objects", "WorkOrders"),
+     os.path.join("eform-client", "e2e", "Page objects", "WorkOrders")),
+    (os.path.join("eform-client", "wdio-headless-plugin-step2.conf.js"),
+     os.path.join("eform-client", "wdio-plugin-step2.conf.js")),
+]
+
+# Copy the test files and directories
+for src_rel_path, dst_rel_path in test_files_to_copy:
+    src_path = os.path.join(src_base, src_rel_path)
+    dst_path = os.path.join(dst_base, dst_rel_path)
+
+    if os.path.isdir(src_path):
+        shutil.copytree(src_path, dst_path)
+    else:
+        shutil.copy2(src_path, dst_path)

upgradeeformnugets.py
@@ -1,0 +1,94 @@
+import os
+import subprocess
+import re
+import requests
+import json
+
+def run_command(command, shell=False, capture_output=False):
+    return subprocess.run(command, shell=shell, check=True, capture_output=capture_output, text=True).stdout.strip()
+
+def get_git_status():
+    return run_command("git status | grep 'nothing to commit, working tree clean' | wc -l", shell=True, capture_output=True)
+
+def get_commit_count():
+    return int(run_command("git log --oneline | wc -l", shell=True, capture_output=True))
+
+def get_package_version(package_name, project_name):
+    output = run_command(f"dotnet list {project_name} package | grep '{package_name} '", shell=True)
+    match = re.search(r'(\d+\.\d+\.\d+)', output)
+    return match.group(0) if match else None
+
+def bump_package_version(project_name, package_name, old_version, new_version, repository):
+    issue_data = {
+        "title": f"Bump {package_name} from {old_version} to {new_version}",
+        "body": "TBD",
+        "assignees": ["renemadsen"],
+        "labels": [".NET", "backend", "enhancement"]
+    }
+
+    headers = {
+        "Authorization": f"token {os.getenv('CHANGELOG_GITHUB_TOKEN')}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(
+        f"https://api.github.com/repos/microting/{repository}/issues",
+        headers=headers,
+        data=json.dumps(issue_data)
+    )
+
+    issue_number = response.json().get("number")
+    run_command(["git", "add", "."])
+    run_command(["git", "commit", "-a", "-m", f"closes #{issue_number}"])
+
+def get_git_version():
+    return run_command("git tag --sort=-creatordate | head -n 1", shell=True).replace('v', '')
+
+def update_git_version():
+    current_version = get_git_version()
+    major, minor, build = map(int, current_version.split('.'))
+    new_version = f"v{major}.{minor}.{build + 1}"
+    run_command(["git", "tag", new_version])
+    run_command(["git", "push", "--tags"])
+    run_command(["git", "push"])
+    return new_version
+
+def process_repository(project_name, packages, repository):
+    for package_name in packages:
+        old_version = get_package_version(package_name, project_name)
+        run_command(["dotnet", "add", project_name, "package", package_name])
+        new_version = get_package_version(package_name, project_name)
+
+        if new_version and new_version != old_version:
+            bump_package_version(project_name, package_name, old_version, new_version, repository)
+
+def main():
+    os.chdir(os.path.expanduser("~"))
+
+    if int(get_git_status()) > 0:
+        run_command("git checkout master", shell=True)
+        run_command("git pull", shell=True)
+        os.chdir("eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn")
+
+        current_number_of_commits = get_commit_count()
+        packages = [
+            'Microting.eForm', 'Microting.eFormApi.BasePn', 'Microting.WorkOrderBase'
+        ]
+        process_repository('WorkOrders.Pn.csproj', packages, 'eform-angular-work-orders-plugin')
+
+        new_number_of_commits = get_commit_count()
+        if new_number_of_commits > current_number_of_commits:
+            new_version = update_git_version()
+            print(f"Updated Microting eForm and pushed new version {new_version}")
+            os.chdir("../../../..")
+            run_command(f"github_changelog_generator -u microting -p eform-angular-work-orders-plugin -t {os.getenv('CHANGELOG_GITHUB_TOKEN')}", shell=True)
+            run_command(["git", "add", "CHANGELOG.md"])
+            run_command(["git", "commit", "-m", "Updating changelog"])
+            run_command(["git", "push"])
+        else:
+            print("Nothing to do, everything is up to date.")
+    else:
+        print("Working tree is not clean, so we are not going to upgrade. Clean before doing upgrade!")
+
+if __name__ == "__main__":
+    main()

microting/eform-angular-inventory-plugin

devgetchanges.py
@@ -1,0 +1,65 @@
+import os
+import shutil
+
+# Change directory to the user's home directory
+os.chdir(os.path.expanduser("~"))
+
+# Define paths
+src_base = os.path.join("Documents", "workspace", "microting", "eform-angular-frontend")
+dst_base = os.path.join("Documents", "workspace", "microting", "eform-angular-inventory-plugin")
+
+# Remove and copy main directories
+paths = [
+    (os.path.join("eform-client", "src", "app", "plugins", "modules", "inventory-pn"),
+     os.path.join("eform-client", "src", "app", "plugins", "modules", "inventory-pn")),
+    (os.path.join("eFormAPI", "Plugins", "Inventory.Pn"),
+     os.path.join("eFormAPI", "Plugins", "Inventory.Pn")),
+]
+
+for dst_rel_path, src_rel_path in paths:
+    dst_path = os.path.join(dst_base, dst_rel_path)
+    src_path = os.path.join(src_base, src_rel_path)
+
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+
+    shutil.copytree(src_path, dst_path)
+
+# List of test files and directories to remove
+test_files_to_remove = [
+    os.path.join("eform-client", "e2e", "Tests", "inventory-settings"),
+    os.path.join("eform-client", "e2e", "Tests", "inventory-general"),
+    os.path.join("eform-client", "wdio-headless-plugin-step2.conf.js"),
+    os.path.join("eform-client", "e2e", "Page objects", "Inventory"),
+]
+
+# Remove the test files and directories
+for rel_path in test_files_to_remove:
+    full_path = os.path.join(dst_base, rel_path)
+    if os.path.exists(full_path):
+        if os.path.isdir(full_path):
+            shutil.rmtree(full_path)
+        else:
+            os.remove(full_path)
+
+# List of test files and directories to copy
+test_files_to_copy = [
+    (os.path.join("eform-client", "e2e", "Tests", "inventory-settings"),
+     os.path.join("eform-client", "e2e", "Tests", "inventory-settings")),
+    (os.path.join("eform-client", "e2e", "Tests", "inventory-general"),
+     os.path.join("eform-client", "e2e", "Tests", "inventory-general")),
+    (os.path.join("eform-client", "wdio-plugin-step2.conf.js"),
+     os.path.join("eform-client", "wdio-headless-plugin-step2.conf.js")),
+    (os.path.join("eform-client", "e2e", "Page objects", "Inventory"),
+     os.path.join("eform-client", "e2e", "Page objects", "Inventory")),
+]
+
+# Copy the test files and directories
+for src_rel_path, dst_rel_path in test_files_to_copy:
+    src_path = os.path.join(src_base, src_rel_path)
+    dst_path = os.path.join(dst_base, dst_rel_path)
+
+    if os.path.isdir(src_path):
+        shutil.copytree(src_path, dst_path)
+    else:
+        shutil.copy2(src_path, dst_path)

devinstall.py
@@ -1,0 +1,76 @@
+import os
+import shutil
+
+# Change directory to the user's home directory
+os.chdir(os.path.expanduser("~"))
+
+# Print the current working directory
+print("Current working directory:", os.getcwd())
+
+# Define paths
+src_base = os.path.join("Documents", "workspace", "microting", "eform-angular-inventory-plugin")
+dst_base = os.path.join("Documents", "workspace", "microting", "eform-angular-frontend")
+
+# Remove and copy directories
+paths = [
+    (os.path.join("eform-client", "src", "app", "plugins", "modules", "inventory-pn"),
+     os.path.join("eform-client", "src", "app", "plugins", "modules", "inventory-pn")),
+    (os.path.join("eFormAPI", "Plugins", "Inventory.Pn"),
+     os.path.join("eFormAPI", "Plugins", "Inventory.Pn")),
+]
+
+# Removing and copying files
+for dst_rel_path, src_rel_path in paths:
+    dst_path = os.path.join(dst_base, dst_rel_path)
+    src_path = os.path.join(src_base, src_rel_path)
+
+    # Remove the destination directory if it exists
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+
+    # Copy the source directory to the destination
+    shutil.copytree(src_path, dst_path)
+
+# Ensure the Plugins directory exists
+plugins_dir = os.path.join(dst_base, "eFormAPI", "Plugins")
+os.makedirs(plugins_dir, exist_ok=True)
+
+# List of test files and directories to remove
+test_files_to_remove = [
+    os.path.join("eform-client", "e2e", "Tests", "inventory-settings"),
+    os.path.join("eform-client", "e2e", "Tests", "inventory-general"),
+    os.path.join("eform-client", "e2e", "Page objects", "Inventory"),
+    os.path.join("eform-client", "wdio-plugin-step2.conf.js"),
+]
+
+# Remove the test files and directories
+for rel_path in test_files_to_remove:
+    full_path = os.path.join(dst_base, rel_path)
+    if os.path.exists(full_path):
+        if os.path.isdir(full_path):
+            shutil.rmtree(full_path)
+        else:
+            os.remove(full_path)
+
+# List of test files and directories to copy
+test_files_to_copy = [
+    (os.path.join("eform-client", "e2e", "Tests", "inventory-settings"),
+     os.path.join("eform-client", "e2e", "Tests", "inventory-settings")),
+    (os.path.join("eform-client", "e2e", "Tests", "inventory-general"),
+     os.path.join("eform-client", "e2e", "Tests", "inventory-general")),
+    (os.path.join("eform-client", "e2e", "Page objects", "Inventory"),
+     os.path.join("eform-client", "e2e", "Page objects", "Inventory")),
+    (os.path.join("eform-client", "wdio-headless-plugin-step2.conf.js"),
+     os.path.join("eform-client", "wdio-plugin-step2.conf.js")),
+]
+
+# Copy the test files and directories
+for src_rel_path, dst_rel_path in test_files_to_copy:
+    src_path = os.path.join(src_base, src_rel_path)
+    dst_path = os.path.join(dst_base, dst_rel_path)
+
+    # Copy the source directory to the destination
+    if os.path.isdir(src_path):
+        shutil.copytree(src_path, dst_path)
+    else:
+        shutil.copy2(src_path, dst_path)

upgradeeformnugets.py
@@ -1,0 +1,94 @@
+import os
+import subprocess
+import re
+import requests
+import json
+
+def run_command(command, shell=False, capture_output=False):
+    return subprocess.run(command, shell=shell, check=True, capture_output=capture_output, text=True).stdout.strip()
+
+def get_git_status():
+    return run_command("git status | grep 'nothing to commit, working tree clean' | wc -l", shell=True, capture_output=True)
+
+def get_commit_count():
+    return int(run_command("git log --oneline | wc -l", shell=True, capture_output=True))
+
+def get_package_version(package_name, project_name):
+    output = run_command(f"dotnet list {project_name} package | grep '{package_name} '", shell=True)
+    match = re.search(r'(\d+\.\d+\.\d+)', output)
+    return match.group(0) if match else None
+
+def bump_package_version(project_name, package_name, old_version, new_version, repository):
+    issue_data = {
+        "title": f"Bump {package_name} from {old_version} to {new_version}",
+        "body": "TBD",
+        "assignees": ["renemadsen"],
+        "labels": [".NET", "backend", "enhancement"]
+    }
+
+    headers = {
+        "Authorization": f"token {os.getenv('CHANGELOG_GITHUB_TOKEN')}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(
+        f"https://api.github.com/repos/microting/{repository}/issues",
+        headers=headers,
+        data=json.dumps(issue_data)
+    )
+
+    issue_number = response.json().get("number")
+    run_command(["git", "add", "."])
+    run_command(["git", "commit", "-a", "-m", f"closes #{issue_number}"])
+
+def get_git_version():
+    return run_command("git tag --sort=-creatordate | head -n 1", shell=True).replace('v', '')
+
+def update_git_version():
+    current_version = get_git_version()
+    major, minor, build = map(int, current_version.split('.'))
+    new_version = f"v{major}.{minor}.{build + 1}"
+    run_command(["git", "tag", new_version])
+    run_command(["git", "push", "--tags"])
+    run_command(["git", "push"])
+    return new_version
+
+def process_repository(project_name, packages, repository):
+    for package_name in packages:
+        old_version = get_package_version(package_name, project_name)
+        run_command(["dotnet", "add", project_name, "package", package_name])
+        new_version = get_package_version(package_name, project_name)
+
+        if new_version and new_version != old_version:
+            bump_package_version(project_name, package_name, old_version, new_version, repository)
+
+def main():
+    os.chdir(os.path.expanduser("~"))
+
+    if int(get_git_status()) > 0:
+        run_command("git checkout master", shell=True)
+        run_command("git pull", shell=True)
+        os.chdir("eFormAPI/Plugins/Inventory.Pn/Inventory.Pn")
+
+        current_number_of_commits = get_commit_count()
+        packages = [
+            'Microting.eForm', 'Microting.eFormApi.BasePn', 'Microting.eFormInventoryBase', 'Magick.NET-Q16-x64'
+        ]
+        process_repository('Inventory.Pn.csproj', packages, 'eform-angular-items-planning-plugin')
+
+        new_number_of_commits = get_commit_count()
+        if new_number_of_commits > current_number_of_commits:
+            new_version = update_git_version()
+            print(f"Updated Microting eForm and pushed new version {new_version}")
+            os.chdir("../../../..")
+            run_command(f"github_changelog_generator -u microting -p eform-angular-items-planning-plugin -t {os.getenv('CHANGELOG_GITHUB_TOKEN')}", shell=True)
+            run_command(["git", "add", "CHANGELOG.md"])
+            run_command(["git", "commit", "-m", "Updating changelog"])
+            run_command(["git", "push"])
+        else:
+            print("Nothing to do, everything is up to date.")
+    else:
+        print("Working tree is not clean, so we are not going to upgrade. Clean before doing upgrade!")
+
+if __name__ == "__main__":
+    main()